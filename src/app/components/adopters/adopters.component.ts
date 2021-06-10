f410a47fbad6666619ed56e64417940f5e82ac1b
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-10 22:36:44 +0300

    add a navbar

--- src/app/components/adopters/adopters.component.ts
+++ src/app/components/adopters/adopters.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Adopter } from '../../interfaces/adopter';
+import { AdopterService } from '../../services/adopter/adopter.service'
+
 
 @Component({
   selector: 'app-adopters',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdoptersComponent implements OnInit {
 
-  constructor() { }
+  adopter: Adopter[] = []
+
+  constructor(private adopterservice: AdopterService) { }
+
+  getAdopter() {
+    this.adopterservice.getAll().subscribe(data => {
+      this.adopter = data;
+      console.log(this.adopter);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  updateAdopter(id: any, adopter: Adopter) {
+    this.adopterservice.update(id, adopter).subscribe((data) => {
+      console.log(adopter.id)
+      console.warn('data', data)
+    })
+    console.warn(id)
+  }
+
+  deleteAdopter(id: any) {
+    this.adopterservice.delete(id).subscribe(response => {
+      console.log(response)
+      this.getAdopter()
+    }, error => {
+      console.log(error)
+    })
+  }
 
   ngOnInit(): void {
+    this.getAdopter()
   }
 
 }

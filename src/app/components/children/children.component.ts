c3ca430652e65c3f688019995330fd87bad9e730
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-10 22:17:53 +0300

    add functions to call in the child objects

--- src/app/components/children/children.component.ts
+++ src/app/components/children/children.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Child } from '../../interfaces/child'
+import { ChildService } from '../../services/child/child.service'
 
 @Component({
   selector: 'app-children',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildrenComponent implements OnInit {
 
-  constructor() { }
+  child: Child[] = []
+
+  constructor(private childservice: ChildService) { }
+
+  getChild(){
+    this.childservice.getAll().subscribe(data=>{
+      this.child = data;
+      console.log(this.child);
+    }, error =>{
+      console.log(error);
+    })
+  }
+
+  updateChild(id: any, child: Child) {
+    this.childservice.update(id, child).subscribe((data) => {
+      console.log(child.id)
+      console.warn('data', data)
+    })
+    console.warn(id)
+  }
+
+  deleteBusiness(id:any){
+    this.childservice.delete(id).subscribe(response => {
+      console.log(response)
+      this.getChild()
+    }, error =>{
+      console.log(error)
+    })
+  }
 
   ngOnInit(): void {
+    this.getChild();
   }
 
 }

6bde35cab53775fb62fff9575a6ebcd3e01efb0e
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-10 18:17:59 +0300

    add child interface

--- src/app/interfaces/child.ts
+++ src/app/interfaces/child.ts
@@ -1,2 +1,15 @@
 export interface Child {
+    id: any;
+    passport_image: any;
+    first_name: string;
+    middle_name: string;
+    surname: string;
+    age: number;
+    adopter: string;
+    sponser: string;
+    gender: string;
+    talent: string;
+    medical_records: any;
+    school_report: any;
+    joined_at: any;
 }

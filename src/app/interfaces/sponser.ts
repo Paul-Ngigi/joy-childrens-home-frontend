6efa2b6c7bd50769b515643ed02df9089e4faf05
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-10 18:20:39 +0300

    add sponser interface

--- src/app/interfaces/sponser.ts
+++ src/app/interfaces/sponser.ts
@@ -1,2 +1,13 @@
 export interface Sponser {
+    id: any;
+    passport_image: any;
+    first_name: string;
+    last_name: string;
+    surname: string;
+    gender: string;
+    email: string;
+    phone_number: number;
+    id_number: number;
+    location: string;
+    marital_status: string;
 }

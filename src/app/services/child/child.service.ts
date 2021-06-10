7435bfca77abad5842b9e8b84747ac7cc9c8deff
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-10 22:03:07 +0300

    add child service

--- src/app/services/child/child.service.ts
+++ src/app/services/child/child.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Child } from '../../interfaces/child'
+
+const url = 'https://joy-children-home.herokuapp.com/api/sponsers/';
+const updateUrl = 'https://joy-children-home.herokuapp.com/api/sponsers/update/';
+const deleteUrl = 'https://joy-children-home.herokuapp.com/api/sponsers/delete/';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ChildService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    console.log(this.http.get(url));
+    return this.http.get(url);
+  }
+
+  getOne(id: any): Observable<any> {
+    return this.http.get(`${url}${id}/`);
+  }
+
+  create(child: Child) {
+    return this.http.post(url, child)
+  }
+
+  update(id: any, child: Child) {
+    return this.http.put(`${updateUrl}${id}/`, child)
+  }
+
+
+  delete(id: any) {
+    return this.http.delete(`${deleteUrl}${id}/`)
+  }
 }

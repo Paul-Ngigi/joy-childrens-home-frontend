1698d56bd73aa2e2ac29cffc01d3d8de807df8ba
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-10 22:23:23 +0300

    add functions to call in the sponser objects

--- src/app/components/sponsers/sponsers.component.ts
+++ src/app/components/sponsers/sponsers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Sponser } from '../../interfaces/sponser';
+import { SponserService } from '../../services/sponser/sponser.service'
+
 
 @Component({
   selector: 'app-sponsers',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SponsersComponent implements OnInit {
 
-  constructor() { }
+  sponser: Sponser[] = []
+
+  constructor(private sponserservice: SponserService) { }
+
+  getSponser() {
+    this.sponserservice.getAll().subscribe(data => {
+      this.sponser = data;
+      console.log(this.sponser);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  updateSponser(id: any, sponser: Sponser) {
+    this.sponserservice.update(id, sponser).subscribe((data) => {
+      console.log(sponser.id)
+      console.warn('data', data)
+    })
+    console.warn(id)
+  }
+
+  deleteSponser(id: any) {
+    this.sponserservice.delete(id).subscribe(response => {
+      console.log(response)
+      this.getSponser()
+    }, error => {
+      console.log(error)
+    })
+  }
 
   ngOnInit(): void {
+    this.getSponser();
   }
 
 }

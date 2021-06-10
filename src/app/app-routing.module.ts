2964d92b43b648ae00fb8e833940bbfa95e30b18
Paul-Ngigi <paulkush7777@gmail.com>
2021-06-10 22:40:13 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ChildrenComponent } from './components/children/children.component'
+import { SponsersComponent } from './components/sponsers/sponsers.component'
+import { AdoptersComponent } from './components/adopters/adopters.component'
+import { NotFoundComponent } from './components/not-found/not-found.component'
+
+
+const routes: Routes = [
+  {path: 'children', component: ChildrenComponent},
+  {path: 'adopters', component: AdoptersComponent},
+  {path: 'sponsers', component: SponsersComponent},
+  { path: '**', component: NotFoundComponent },
+
+  // { path: '', redirectTo:"", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
